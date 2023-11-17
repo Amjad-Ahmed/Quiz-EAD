bddc8fe48f7cfe85aaf318425b9df93e1d05cde8
Amjad Ahmed <112887280+Amjad-Ahmed@users.noreply.github.com>
2023-11-17 17:04:13 +0500

    Add files via upload

---
+++ src/components/AllProjects.js
@@ -0,0 +1,162 @@
+
+import image from '../assets/images/a.jpg'
+function AllProjects() {
+    return (
+       <div className={"bg-dark text-white"}>
+           <div className="row d-flex align-items-center justify-content-between container">
+               <div className="col-sm-12 col-lg-6 col-md-12">
+                   <h1>Menu</h1>
+               </div>
+           </div>
+            <div className="container row">
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                                <p className="card-text">Some quick example text to build on the card title and make up
+                                    the bulk of the card's content.</p>
+                                <a href="#" className="btn btn-dark">Order</a>
+                            </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 pt-4 pb-4 col-md-6 col-lg-3">
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="card-img-top" src={image} alt="Card image cap" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <img height={100} width={100} src="https://avatars.githubusercontent.com/u/112887280?s=96&v=4" alt=""/>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-dark">Order</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+       </div>
+    );
+}
+
+export default AllProjects;
